d0b29eec09ce480ce9a5deb02ee2c5a3bc1c0be2
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-11-28 22:52:47 +0000

    linux (RPi): update to linux-963f09b (4.20-rc4)

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -38,8 +38,8 @@ case "$LINUX" in
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
   raspberrypi)
-    PKG_VERSION="decf73f38fb1b62223b04554f6e51715540640c4" # 4.20-rc3
-    PKG_SHA256="855ceb3939c4bf336771f58d2d956396766024802454ad157b92e6a56d575922"
+    PKG_VERSION="963f09bd46b0e37b8066cd47202fa0809eef52c9" # 4.20-rc4
+    PKG_SHA256="f75125d2878f9f82174c9c78e2456ac1c7ea4264aabe6877c4d8d930432dde39"
     PKG_URL="https://github.com/raspberrypi/linux/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
